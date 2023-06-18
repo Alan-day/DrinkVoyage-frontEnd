6371b36764882c37f5b5b56c048a02d74d83d99f
Alan <lesterthenovice@gmail.com>
2023-06-18 21:24:13 +0100

    slightly improved styling

--- src/Components/Searchbox/Searchbox.jsx
+++ src/Components/Searchbox/Searchbox.jsx
@@ -1,64 +1,86 @@
 import React from "react";
 import "./Searchbox.scss";
 
-const Searchbox = ({ searchTerm, handleInput, setWine, setBeer, setRakia, setVodka, setWhiskey, setOther }) => {
-return (
-  <>
-<form className="search-box">
-<label className="search-box__label">Search by name:</label>
-<input
-     type="text"
-     value={searchTerm}
-     onChange={handleInput}
-     className="search-box__input"
-   />
-</form>
+const Searchbox = ({
+  searchTerm,
+  handleInput,
+  setWine,
+  setBeer,
+  setRakia,
+  setVodka,
+  setWhiskey,
+  setOther,
+}) => {
+  return (
+    <>
+      <form className="search-box">
+        <label className="search-box__label">Search by name:</label>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleInput}
+          className="search-box__input"
+        />
+      </form>
 
-<div className="search-box__selection">
+      <div className="search-box__selection">
+        
+        <label className="item">
+          Wine
+          <input
+            type="checkbox"
+            value="wine"
+            onChange={setWine}
+          />
+        </label>
 
-<label>
-    Wine
-        <input type="checkbox" value="wine" onChange={setWine}></input>
-      </label>
+        <label className="item">
+          Whiskey
+          <input
+            type="checkbox"
+            value="whiskey"
+            onChange={setWhiskey}
+          />
+        </label>
 
-      <label>
-       Whiskey
-        <input type="checkbox" value="whiskey" onChange={setWhiskey}></input>
-      </label>
+        <label className="item">
+          Vodka
+          <input
+            type="checkbox"
+            value="vodka"
+            onChange={setVodka}
+          />
+        </label>
 
-      <label>
-        Vodka
-        <input type="checkbox" value="vodka" onChange={setVodka}></input>
-      </label>
+        <label className="item">
+          Beer
+          <input
+            type="checkbox"
+            value="beer"
+            onChange={setBeer}
+          />
+        </label>
 
-      <label>
-         Beer
-        <input type="checkbox" value="beer" onChange={setBeer}></input>
-      </label>
+        <label className="item">
+          Rakia/Rakija
+          <input
+            type="checkbox"
+            value="rakia"
+            onChange={setRakia}
+          />
+        </label>
 
-
-      <label>
-        Rakia/Rakija
-        <input type="checkbox" value="rakia" onChange={setRakia}></input>
-
-      </label>
-
-
-      <label>
-        Other
-        <input type="checkbox" value="other" onChange={setOther}></input>
-
-      </label>
-
-
-
-
-</div>
-
-
-</>
-
-);
+        <label className="item">
+          Other
+          <input
+            type="checkbox"
+            value="other"
+            onChange={setOther}
+          />
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default Searchbox;
