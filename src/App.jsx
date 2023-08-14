960a7c9d635e49c661ecd2dcb9bba5a8f3b565af
Alan-day <lesterthenovice@gmail.com>
2023-08-14 14:33:39 +0100

    used optional changing for filteredAlcohol function

--- src/App.jsx
+++ src/App.jsx
@@ -15,213 +15,148 @@ function App() {
 
   useEffect(() => {
     getList();
-  
   }, []);
 
   const getList = async () => {
-  
-      const url = "http://localhost:8080/list";
-      const response = await fetch(url);
-      const list = await response.json();
-      setAlcoholList(list);
-    
+    const url = "http://localhost:8080/list";
+    const response = await fetch(url);
+    const list = await response.json();
+    setAlcoholList(list);
   };
-
-
 
   const getBeer = async () => {
-    
-      const url = "http://localhost:8080/list/beers";
-      const response = await fetch(url);
-      const listBeer = await response.json();
-      setAlcoholList(listBeer);
-   
-
+    const url = "http://localhost:8080/list/beers";
+    const response = await fetch(url);
+    const listBeer = await response.json();
+    setAlcoholList(listBeer);
   };
 
-
   const getWine = async () => {
-    
     const url = "http://localhost:8080/list/wines";
     const response = await fetch(url);
     const listWine = await response.json();
     setAlcoholList(listWine);
- 
+  };
 
-};
+  const getVodka = async () => {
+    const url = "http://localhost:8080/list/vodkas";
+    const response = await fetch(url);
+    const listVodkas = await response.json();
+    setAlcoholList(listVodkas);
+  };
 
-const getVodka = async () => {
-    
-  const url = "http://localhost:8080/list/vodkas";
-  const response = await fetch(url);
-  const listVodkas = await response.json();
-  setAlcoholList(listVodkas);
+  const getRakia = async () => {
+    const url = "http://localhost:8080/list/rakias";
+    const response = await fetch(url);
+    const listRakias = await response.json();
+    setAlcoholList(listRakias);
+  };
 
+  const getWhiskey = async () => {
+    const url = "http://localhost:8080/list/whiskeys";
+    const response = await fetch(url);
+    const listWhiskeys = await response.json();
+    setAlcoholList(listWhiskeys);
+  };
 
-};
-
-
-const getRakia = async () => {
-    
-  const url = "http://localhost:8080/list/rakias";
-  const response = await fetch(url);
-  const listRakias = await response.json();
-  setAlcoholList(listRakias);
-
-
-};
-
-const getWhiskey = async () => {
-    
-  const url = "http://localhost:8080/list/whiskeys";
-  const response = await fetch(url);
-  const listWhiskeys = await response.json();
-  setAlcoholList(listWhiskeys);
-
-
-};
-
-
-
-const getOthers = async () => {
-    
-  const url = "http://localhost:8080/list/others";
-  const response = await fetch(url);
-  const listOthers = await response.json();
-  setAlcoholList(listOthers);
-
-
-};
-
-
-
-
-
-
-
-
+  const getOthers = async () => {
+    const url = "http://localhost:8080/list/others";
+    const response = await fetch(url);
+    const listOthers = await response.json();
+    setAlcoholList(listOthers);
+  };
 
   const handleBeer = (event) => {
     if (event.target.value === "beer" && event.target.checked === true) {
       getBeer();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
-
 
   const handleWine = (event) => {
     if (event.target.value === "wine" && event.target.checked === true) {
       getWine();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
 
   const handleWhiskey = (event) => {
     if (event.target.value === "whiskey" && event.target.checked === true) {
       getWhiskey();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
-
 
   const handleRakias = (event) => {
     if (event.target.value === "rakia" && event.target.checked === true) {
       getRakia();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
 
   const handleVodkas = (event) => {
     if (event.target.value === "vodka" && event.target.checked === true) {
       getVodka();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
 
   const handleOthers = (event) => {
     if (event.target.value === "other" && event.target.checked === true) {
       getOthers();
-     
     } else {
-  getList();
+      getList();
     }
   };
-
-
-
-
-
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-
-
-  
   const filteredAlcohol = alcoholList.filter((alco) => {
-  const alcoholToLower = alco.originCountry.toLowerCase();
-
+    const alcoholToLower = alco.originCountry?.toLowerCase() || ""; // optional changing used to check if variable is null before calling
     return alcoholToLower.includes(searchTerm.toLowerCase());
   });
 
-
-
-
-
   return (
-<Router>
-  <div className="App">
-    <WelcomeBanner />
-    <Navbar  />
-    <div className="card-container">
-      <Routes>
-        <Route
-          path="/"
-          element={<Homepage  />}
-        />
-        <Route
-          path="/list"
-          element={
-            <>
-            <div className="card-container__search">
-              <Searchbox
-                setBeer={handleBeer}
-                setOther={handleOthers}
-                setVodka={handleVodkas}
-                setRakia={handleRakias}
-                setWine={handleWine}
-                setWhiskey={handleWhiskey}
-                handleInput={handleSearch}
-                searchTerm={searchTerm}
-              />
-              <AlcoholCardsList data={filteredAlcohol} />
-              </div>
-            </>
-          }
-        />
-        <Route path="/createDrink" element={<CreateAlcoholCard />} />
-        <Route path="/list/edit/:id" element={<EditAlcoholCard />} />
-      </Routes>
-    </div>
-  </div>
-</Router>
-
+    <Router>
+      <div className="App">
+        <WelcomeBanner />
+        <Navbar />
+        <div className="card-container">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route
+              path="/list"
+              element={
+                <>
+                  <div className="card-container__search">
+                    <Searchbox
+                      setBeer={handleBeer}
+                      setOther={handleOthers}
+                      setVodka={handleVodkas}
+                      setRakia={handleRakias}
+                      setWine={handleWine}
+                      setWhiskey={handleWhiskey}
+                      handleInput={handleSearch}
+                      searchTerm={searchTerm}
+                    />
+                    <AlcoholCardsList data={filteredAlcohol} />
+                  </div>
+                </>
+              }
+            />
+            <Route path="/createDrink" element={<CreateAlcoholCard />} />
+            <Route path="/list/edit/:id" element={<EditAlcoholCard />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/CreateAlcoholCard/CreateAlcoholCard.jsx
+++ src/Components/CreateAlcoholCard/CreateAlcoholCard.jsx
@@ -1,20 +1,16 @@
-
 import "./CreateAlcoholCard.scss";
 import { useNavigate } from "react-router-dom";
 import Form from "../Form/Form";
 const CreateAlcoholCard = () => {
   const navigate = useNavigate();
 
-
-  const handleSubmit = async newDrink => {
- 
-
+  const handleSubmit = async (newDrink) => {
     const result = await fetch("http://localhost:8080/createDrink", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newDrink), 
+      body: JSON.stringify(newDrink),
     });
 
     if (result.ok) {
@@ -23,16 +19,20 @@ const CreateAlcoholCard = () => {
       navigate("/");
     }
   };
-  const defaultFormState = { name: "",  description:"", category: "", originCountry:""};
+  const defaultFormState = {
+    name: "",
+    description: "",
+    category: "",
+    originCountry: "",
+    flag: "",
+  };
 
   return (
     <section className="create-drink">
-    
       <Form
         handleSubmit={handleSubmit}
         defaultFormState={defaultFormState}
         formTitle="Add A New Drink"
-    
       />
     </section>
   );
